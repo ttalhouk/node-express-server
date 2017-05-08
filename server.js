d183954d81d15a1e1111bace8d7a19e4dc8890fe
Talal Talhouk <talal@sbcglobal.net>
2017-05-08 15:46:26 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+})
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFileSync('server.log', log + '\n');
+  console.log(log);
+  next();
+})
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
+
+app.get('/', (req, res)=>{
+  //res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeText: 'Hello and welcome to our site',
+    name: 'Talal',
+    likes: [
+      'hockey',
+      'code'
+      ]
+  })
+});
+
+app.get('/about', (req,res) =>{
+  res.render('about.hbs', {
+    pageTitle:'About Page',
+  })
+})
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: 'Error processing request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('running on port 3000');
+});
